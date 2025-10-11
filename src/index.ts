dc318b8ecdde588c1f963a17a5c6c115950a5cc3
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-10-10 21:35:18 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { run } from './main';
 import * as core from '@actions/core';
 try{
+    // Run config and project scan requests
     run();
 } catch (error) {
     core.setFailed(`Action failed with error ${error}`);
