0880313ad9eef7168231c5e074e80d8bd637896f
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-10-14 13:43:24 -0500

    added logging for when run function is called

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { run } from './main';
 import * as core from '@actions/core';
 try{
     // Run config and project scan requests
+    core.info('Beginning updates..')
     run();
 } catch (error) {
     core.setFailed(`Action failed with error ${error}`);
